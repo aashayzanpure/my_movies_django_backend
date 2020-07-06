33b2306d410dab4495164a0d7b39b20f9978e1cd
aashayzanpure <aashayz28@gmail.com>
2020-07-06 14:41:43 +0530

    secret keys encapsulated

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a2i5pqs%--%k0!98eizx!$=uh5s++ln+(5+zji6%^m^$1y=ou='
+SECRET_KEY = os.environ.get("DJANGO_ANG_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
